b948e5db1b9872aec4485374a0f62a874485eb88
StuckFanatic <157004704+StuckFanatic@users.noreply.github.com>
2026-02-12 12:03:38 -0600

    Tile and TileType
    Colors allow the tiles to have shape to the future of the game and ability to design them with effects

---
+++ CapstoneGameProjectAudyTisouk/src/adventuresInJava/Tile.java
@@ -0,0 +1,67 @@
+package adventuresInJava;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Tile {
+
+	//Player should not be able to pass water or other types of terrain
+	TileType type;
+	boolean passable;
+	
+	
+	public Tile(TileType type) {
+		
+		
+		this.type = type;
+		
+		//Water should not be passable
+		switch(type) {
+		
+		case WATER:
+			passable = false;
+			break;
+			
+		default:
+			passable = true;
+					
+		}
+			
+	}
+
+	//Instead of black tiles this will color in tiles as placeholder to what they are
+	public void draw(Graphics g, int x, int y, int tileSize) {
+		
+		
+		switch(type) {
+		
+		case GRASS:
+			g.setColor(Color.green);
+			break;
+		case WATER:
+			g.setColor(Color.blue);
+			break;
+		case HILL:
+			g.setColor(Color.gray);
+			break;
+			
+		
+		}
+		
+		g.fillRect(x, y, tileSize, tileSize);
+		g.setColor(Color.black);
+		g.drawRect(x, y, tileSize, tileSize);
+		
+
+	}
+	
+	public boolean isPassable() {
+		
+		return passable;
+		
+	}
+	
+	
+	
+}
---
+++ CapstoneGameProjectAudyTisouk/src/adventuresInJava/TileType.java
@@ -0,0 +1,14 @@
+package adventuresInJava;
+
+public enum TileType {
+
+	GRASS,
+	WATER,
+	HILL
+	
+	
+	
+	// This will define what type of terrain is available 
+	//This can be expanded?
+	
+}
