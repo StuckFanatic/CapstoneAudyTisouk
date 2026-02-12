dc20ff4651755323fcd814c4304f35a002b39e61
StuckFanatic <157004704+StuckFanatic@users.noreply.github.com>
2026-02-12 12:18:32 -0600

    Update on GamePanel
    This allows us to use and see how the design of the tiles will work. replacing the old black backround in favor or a colored generated one for now

--- CapstoneGameProjectAudyTisouk/src/adventuresInJava/GamePanel.java
+++ CapstoneGameProjectAudyTisouk/src/adventuresInJava/GamePanel.java
@@ -20,6 +20,9 @@ public class GamePanel extends JPanel implements Runnable {
     Player player;
 
     Thread gameThread;
+    
+    Tile[][] worldMap;
+    
 
     public GamePanel() {
 
@@ -29,10 +32,37 @@ public class GamePanel extends JPanel implements Runnable {
         
         player = new Player(tileSize);
         
+        worldMap = new Tile[maxScreenCol][maxScreenRow];
+        generateWorld();
+        
        
     }
-
     
+    //For now we will randomly generate the world with tiles to see it visually
+    //The plan will be to generate the world with a text file or hard code it
+    public void generateWorld() {
+    	
+    	for(int col = 0; col < maxScreenCol; col++) {
+    		for( int row = 0; row <maxScreenRow; row++) {
+    			
+    			
+    			if((col + row) % 7 == 0) {
+    				worldMap[col][row] = new Tile(TileType.WATER);
+    				
+    			}else if((col + row) % 5 == 0) {
+    				worldMap[col][row] = new Tile(TileType.HILL);
+    				
+    			}else {
+    				worldMap[col][row] = new Tile(TileType.GRASS);
+    				
+    			}	
+    			
+    		}
+    		
+    		
+    	}	
+    	
+    }
     //Game Logic goes here
     public void startGameThread() {
         gameThread = new Thread(this);
@@ -52,7 +82,6 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     
-    //Tiles and colors here?
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -60,16 +89,15 @@ public class GamePanel extends JPanel implements Runnable {
         g.setColor(Color.WHITE);
         
         //Grid Tiles lines here
+        //Updated with the new TileTypes
         for(int col = 0; col <maxScreenCol; col++) {
         	
         	for( int row = 0; row <maxScreenRow; row++) {
         		
-        		
         		int x = col * tileSize;
         		int y = row * tileSize;
-        		
-        		g.drawRect(x, y, tileSize, tileSize);
-        		
+
+        		worldMap[col][row].draw(g, x, y, tileSize);
         	}
         	
         }
